e51161d4c1da922cf413db112ddb7f65facd4840
Abdelghafour Bamoula <81409058+abdelghafourbamoula@users.noreply.github.com>
2023-02-15 23:57:02 +0100

    EDA - R

--- R_EDA_Example/ex_eda.R
+++ R_EDA_Example/ex_eda.R
@@ -4,10 +4,10 @@ attach(faithful)
 summary(eruptions)
 fivenum(eruptions) # résumé à 5 nombres de Tukey
 
-> mean(eruptions)
-> median(eruptions)
-> min(eruptions)
-> range(eruptions)
-> quantile(eruptions)
-> var(eruptions) # variance
-> sqrt(var(eruptions)) # écart type
+mean(eruptions)
+median(eruptions)
+min(eruptions)
+range(eruptions)
+quantile(eruptions)
+var(eruptions) # variance
+sqrt(var(eruptions)) # écart type
